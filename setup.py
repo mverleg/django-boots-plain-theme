0cfdafa066369a0791c1ab67adf240e213f0263e
rafiki <mark@rafiki>
2016-02-19 08:39:17 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 with open('README.rst', 'r') as fh:
 	readme = fh.read()
 
 setup(
-	name='django_avem_theme',  # should have used a - sorry
+	name='django_avem_theme',
 	description='Simple bootstrap3 theme for Django',
 	long_description=readme,
 	url='https://github.com/mverleg/django_avem_theme',
@@ -16,28 +16,16 @@ setup(
 	author_email='mdilligaf@gmail.com',
 	license='Revised BSD License (LICENSE.txt)',
 	keywords=['django', 'bootstrap'],
-	version='1.1',
+	version='1.2.1',
 	packages=['avem_theme'],
 	include_package_data=True,
 	zip_safe=False,
 	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
+		'Development Status :: 2 - Pre-Alpha',
 		'License :: OSI Approved :: BSD License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: Implementation :: PyPy',
-		'Topic :: Software Development :: Libraries :: Python Modules',
 	],
 	install_requires=[
 		'django',
-		#'beautifulsoup4',
 	],
 )
 
